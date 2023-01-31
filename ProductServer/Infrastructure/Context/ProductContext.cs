74d743edf937ab45cb363ce4171e972901772d9e
Kayin <34983642+kayinleong@users.noreply.github.com>
2023-01-31 23:18:13 +0800

    Update missing files

---
+++ ProductServer/Infrastructure/Context/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductServer.Infrastructure.Models;
+
+namespace ProductServer.Infrastructure.Context;
+
+public class ProductContext : DbContext
+{
+    public ProductContext(DbContextOptions<ProductContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
---
+++ ProductServer/Infrastructure/Models/Product.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using SharedLibrary.Infrastructure.DataTransferObjects;
+using SharedLibrary.Infrastructure.Models;
+using SharedLibrary.Infrastructure.Requests;
+
+namespace ProductServer.Infrastructure.Models;
+
+public class Product : ModelBase
+{
+    [Required] public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal(9, 2)")]
+    public decimal OriginalPrice { get; set; }
+
+    [Required]
+    [Column(TypeName = "decimal(9, 2)")]
+    public decimal DiscountPercentage { get; set; }
+
+    public string ImageFileName { get; set; }
+
+    public static implicit operator ProductDto(Product product)
+    {
+        return new ProductDto
+        {
+            Name = product.Name,
+            Description = product.Description,
+            OriginalPrice = product.OriginalPrice,
+            DiscountPercentage = product.DiscountPercentage,
+            ImageFileName = product.ImageFileName
+        };
+    }
+
+    public static explicit operator Product(ProductRequest productDto)
+    {
+        return new Product
+        {
+            Name = productDto.Name,
+            Description = productDto.Description,
+            OriginalPrice = productDto.OriginalPrice,
+            DiscountPercentage = productDto.DiscountPercentage,
+            ImageFileName = productDto.ImageFileName
+        };
+    }
+}
